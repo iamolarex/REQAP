577e75e1089170f5b053ee598df4edb046eb238e
David Onuh <youngstizy@gmail.com>
2020-03-13 12:16:42 +0100

    initial project setup

---
+++ main/settings/production_settings.py
@@ -0,0 +1,22 @@
+import os
+from .common import *
+
+SECRET_KEY = 'va7kfak0*c_u!qh1hvw8y7_ar3vonyl*u+2qxr51i#5k$p%35r'
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
